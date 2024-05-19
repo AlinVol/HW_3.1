9330ec9653b9efeec3b7d52ba1d62e592c31d0a8
AlinVol <alina.voloshyna@gmail.com>
2024-05-19 18:20:24 +0200

    updated

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
-num1 = int(input("Enter the first number: "))
-        num2 = int(input("Enter the second number: "))
-        action = input("Enter the action (+, -, *, /): ")
+number1 = int(input("Enter the first number: "))
+number2 = int(input("Enter the second number: "))
+action = input("Enter the action (+, -, *, /): ")
+if action == '+':
+            result = number1 + number2
+elif action == '-':
+            result = number1 - number2
+elif action == '*':
+            result = number1 * number2
+elif action == '/':
+   if number2 != 0:
+       result = number1 / number2
+   else:
+       result = "Error: Division by zero!"
+else:
+    result = "Error: Invalid action!"
+print("Result:", result)
